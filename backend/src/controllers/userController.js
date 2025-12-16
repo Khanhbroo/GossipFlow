7907459709f08720ce9168bf438f899158f5b6d8
Khanhbroo <khanhmathias532211@gmail.com>
2025-12-16 23:00:14 +0700

    feat: adding authMe api for extracting user data

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,3 +1,10 @@
 export const authMe = async (req, res) => {
-  return res.status(200).json({ message: "User" });
+  try {
+    const user = req.user; // Take from authMiddleware
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.log("Error when calling authMe", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
