698d70ef373f48aaaa6c10a7b30a622c7117a60b
Khanhbroo <khanhmathias532211@gmail.com>
2025-12-17 19:01:43 +0700

    feat: config route and tailwindcss for front end folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
+
 function App() {
-  return <>App page</>;
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/* Public Route */}
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+
+          {/* Protected Route */}
+          <Route path="/" element={<ChatAppPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
