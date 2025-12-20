a868bea37765f0a2026a7ef114e7f664151cec11
Khanhbroo <khanhmathias532211@gmail.com>
2025-12-20 15:53:58 +0700

    feat: adding types for typescript infering

--- frontend/src/stores/useAuthStore.ts
+++ frontend/src/stores/useAuthStore.ts
@@ -1,26 +1,28 @@
 import { create } from "zustand";
 import { toast } from "sonner";
 import { authService } from "@/services/authService";
+import type { AuthState } from "@/types/store";
 
-export const useAuthStore = create((set, get) => {
-    accessToken: null,
-    user: null,
-    loading: false,
+export const useAuthStore = create<AuthState>((set, get) => ({
+  accessToken: null,
+  user: null,
+  loading: false,
 
-    signUp: async (username, password, email, firstName, lastName) => {
-        try {
-            set({loading: true})
+  signUp: async (username, password, email, firstName, lastName) => {
+    try {
+      set({ loading: true });
 
-            // Call API
-            await authService.signUp(username, password, email, firstName, lastName)
-            
-            toast.success("Signed up successfully! You will be redirected to sign in page")
-        } catch (error) {
-            console.log(error);
-            toast.error("Failed to sign up")
-        }
-        finally {
-            set({loading: false})
-        }
+      // Call API
+      await authService.signUp(username, password, email, firstName, lastName);
+
+      toast.success(
+        "Signed up successfully! You will be redirected to sign in page"
+      );
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to sign up");
+    } finally {
+      set({ loading: false });
     }
-})
+  },
+}));
