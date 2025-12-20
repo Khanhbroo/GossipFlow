d1e2900d4c2dda88fb69d3a5c6b457d129b9ac3a
Khanhbroo <khanhmathias532211@gmail.com>
2025-12-20 15:42:21 +0700

    feat: create useAuthStore hook for taking api from backend server

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
+  withCredentials: true, // Cookie will be sent to server
+});
+
+export default api;
---
+++ frontend/src/stores/useAuthStore.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { toast } from "sonner";
+import { authService } from "@/services/authService";
+
+export const useAuthStore = create((set, get) => {
+    accessToken: null,
+    user: null,
+    loading: false,
+
+    signUp: async (username, password, email, firstName, lastName) => {
+        try {
+            set({loading: true})
+
+            // Call API
+            await authService.signUp(username, password, email, firstName, lastName)
+            
+            toast.success("Signed up successfully! You will be redirected to sign in page")
+        } catch (error) {
+            console.log(error);
+            toast.error("Failed to sign up")
+        }
+        finally {
+            set({loading: false})
+        }
+    }
+})
